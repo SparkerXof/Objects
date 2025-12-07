30464b779c7dd8f824e45bed3d3836bb14107604
SparkerXof <sparket15@gmail.com>
2025-12-07 20:50:04 +0300

    GUI app

--- app/app.py
+++ app/app.py
@@ -1,59 +1,59 @@
+import cv2 as cv
+from PIL import Image, ImageTk
+import numpy as np
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk
+from detector.inference import run_inference
 
-class App:
-    def __init__(self, root: tk.Tk):
+class App():
+    def __init__(self, root, model=None, device='cpu', *args, **kwargs):
         self.root = root
-        self.root.title("Object on Surface Classifier")
-        self.root.geometry("600x600")
-        self.image_w = 400
-        self.image_h = 300
 
-        self.create_widgets()
+        self.model = model
+        self.device = device
 
-    def create_widgets(self):
-        self.image = tk.Label(
-            self.root, 
-            width=10,
-            height=10,
-            bg='gray',
-        )
-        self.image.pack()
+        self.imagepath = None
+        self.image = None
+        self.boxed_image = None
 
-        self.object_class = tk.Label(
-            self.root,
-            text="",
-            font=('Arial', 12)
-        )
-        self.object_class.pack()
+        self.canvas = tk.Canvas(self.root, width=640, height=480, bg = 'gray')
+        self.canvas.grid(column=0, row=0)
 
-        self.load_image = tk.Button(
-            self.root,
-            text="Load an image",
-            command=self.select_image,
-            font=("Arial", 12),
-            bg='lightgray'
-        )
-        self.load_image.pack()
+        self.frame = tk.Frame(self.root)
+        self.frame.grid(column=0, row=1)
 
-    def select_image(self):
-        filepath = filedialog.askopenfilename(
-            title="Select an image",
-            filetypes=[
-                ("Images", "*.jpg *.jpeg *.png *.gif *.bmp"),
-                ("All files", "*.*")
-            ]
-        )
-        if filepath:
-            self.display_image(filepath)
+        self.upload_button = tk.Button(self.frame, text="Upload Image", command=self.upload_image)
+        self.upload_button.grid(column=0, row=0, sticky='w', padx=5, pady=5)
+
+        self.inference_button = tk.Button(self.frame, text="Find boxes", command=self.inference_image)
+        self.inference_button.grid(column=1, row=0, sticky='w', padx=5, pady=5)
     
-    def display_image(self, filepath):
-        try:
-            img = Image.open(filepath)
-            img.thumbnail((300, 300))
-            photo = ImageTk.PhotoImage(img)
-            self.image.configure(image=photo)
-            self.image.image = photo
-        except:
-            self.object_class.configure(text="Image load error")
+    def draw_bboxes(self, img, bboxes, labels, scores, threshold):
+        cv_img = np.array(img)[:, :, ::-1].copy()
+        for i in range(len(labels)):
+            if scores[i] > threshold:
+                x, y, w, h = bboxes[i].astype('int')
+                color = (0, 255, 0) if labels[i] == 1 else (0, 0, 255)
+
+                cv.rectangle(cv_img, (x, y), (w, h), color, 3)
+        return cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
+
+    def image_path(self) -> str:
+        filetypes = [("Image files", "*.png;*.jpg;*.jpeg;*.bmp"), ("All files", '*.*')]
+        return filedialog.askopenfilename(filetypes=filetypes)
+    
+    def upload_image(self):
+        self.imagepath = self.image_path()
+        self.image = ImageTk.PhotoImage(Image.open(self.imagepath).resize((640, 480)))
+        self.canvas.create_image(1, 1, image=self.image, anchor=tk.NW)
+
+    def inference_image(self):
+        img = Image.open(self.imagepath)
+        res = run_inference(self.model, img, self.device)
+        self.boxed_image = Image.fromarray(self.draw_bboxes(img=img, 
+                                                            bboxes=res[0]['boxes'].numpy(), 
+                                                            labels=res[0]['labels'].numpy(), 
+                                                            scores=res[0]['scores'].numpy(), 
+                                                            threshold=0.9))
+        self.boxed_image = ImageTk.PhotoImage(self.boxed_image.resize((640, 480)))
+        self.canvas.create_image(1, 1, image=self.boxed_image, anchor=tk.NW)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import torch
 import tkinter as tk
-from PIL import Image
 from app.app import App
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-from detector.inference import run_inference
 from detector.model import get_model
 
 if __name__ == "__main__":
@@ -13,11 +10,6 @@ if __name__ == "__main__":
     model.load_state_dict(torch.load('./trained.pt', weights_only=True, map_location=device))
     model.eval()
 
-    #root = tk.Tk()
-    #app = App(root)
-    #root.mainloop()
-
-    img = Image.open("/home/sparkerxof/MTUCI/Objects/test.jpg")
-
-    res = run_inference(model, img, device)
-    print(res)
+    root = tk.Tk()
+    app = App(root, model, device)
+    root.mainloop()
